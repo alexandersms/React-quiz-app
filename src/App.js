4413c192fa0388a4771cba960072943c66be871c
Alex <smsxander@gmail.com>
2020-03-31 01:22:49 +0200

    creation de la HomePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Route path="/" component={Home}/>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@mdi/font/css/materialdesignicons.min.css';
+import './styles/styles.scss';
 import App from './App';
 
 ReactDOM.render(
