69e2dfbd52d105d3ab06320b705975ad37ca6343
Alex <smsxander@gmail.com>
2020-03-31 04:00:09 +0200

    stylisation de la homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@mdi/font/css/materialdesignicons.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 import './styles/styles.scss';
 import App from './App';
 
